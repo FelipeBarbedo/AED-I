86a5e30b193c8d37a4844aede21185037193d7be
Felipe <fbbarbedo@inf.ufpel.edu.br>
2019-08-30 13:51:41 -0300

    1 - agenda

---
+++ agenda.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct spa {
+
+    char *nome;
+    int numero;
+    struct spa *next;
+
+};
+
+typedef struct spa space;
+
+space *begin();
+void add(space *first);
+void print(space *second);
+
+int main() {
+
+    space *second;
+
+    printf("Teste\n\n");
+    second = begin();
+    add(second);
+    print(second);
+    add(second);
+    print(second);
+
+    return 0;
+}
+
+space *begin() {
+
+    space *start;
+
+    start = (space *) malloc(sizeof(space));
+    start->next = NULL;
+    return start;
+
+}
+
+void add(space *first) {
+
+    space *new;
+    char *nome;
+    int numero;
+    
+    printf("\nDigite o nome: ");
+    fgets(nome, 100, stdin);
+    printf("\nDigite o telefone: ");
+    scanf("%d", &numero);
+    new = (space *) malloc(sizeof(space));
+    new->nome = nome;
+    new->numero = numero;
+    new->next = NULL;
+    first->next = new;  
+
+}
+
+void print(space *second) {
+
+    space *first;
+
+    for(first = second->next; first != NULL; first = first->next) {
+        printf("\nNome: %s", first->nome);
+        printf("\nNumero: %d", first->numero);
+
+    }
+
+}
